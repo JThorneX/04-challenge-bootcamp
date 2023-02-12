0077606a50cbc535221b7ae09690c54c65ffede9
K'G <idragons1720@gmail.com>
2023-02-11 20:18:36 -0700

    very basic layout html + css

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+var generateBtn = document.querySelector("#submit");
+var questionEl = document.querySelector("#questions");
+var answerEl = document.querySelector("#answers");
+var questionsCont = document.querySelector("#questionsContainer");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+generateBtn.addEventListener("click");
